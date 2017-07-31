bfac63260daaed6debb0a59aad2c4af90160b359
yiwang <1490774017@qq.com>
2017-07-31 16:54:35 +0800

    helloworld
    测试项目helloworld代码上传

---
+++ src/com/wangyi/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.wangyi.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		System.out.println("你好 世界！");
+	}
+
+}
