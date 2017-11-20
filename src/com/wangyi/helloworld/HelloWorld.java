8d233b9fd62d0ab8415b4bf04fc168bbaa4a3af4
yiwang <30490204+yiwang1@users.noreply.github.com>
2017-11-20 22:08:08 +0800

    而是git提交代码

--- src/com/wangyi/helloworld/HelloWorld.java
+++ src/com/wangyi/helloworld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 		System.out.println("你好 世界！");
+		System.out.println("修改代码，然后提交，看看是否有变化");
 	}
 
 }
